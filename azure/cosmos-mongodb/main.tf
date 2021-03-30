4a0c266ff46540e754cf68f6c7e2bff7876fe095
Murugan <krpmurugan@gmail.com>
2021-03-30 16:37:25 +0530

    Cosmos mongodb samples

---
+++ azure/cosmos-mongodb/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_version = ">= 0.12.6"
+  required_providers {
+    azurerm = {
+      version = "~> 2.53.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id            = var.subscription_id
+  skip_provider_registration = true
+}
+
+resource "azurerm_cosmosdb_account" "acc" {
+
+  name                      = var.cosmos_db_account_name
+  location                  = var.resource_group_location
+  resource_group_name       = var.resource_group_name
+  offer_type                = "Standard"
+  kind                      = "MongoDB"
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level = "BoundedStaleness"
+    max_interval_in_seconds = 400
+    max_staleness_prefix    = 200000
+  }
+
+  geo_location {
+    location          = var.failover_location
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = var.resource_group_location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "mongodb" {
+  name                = "mrfirstcosmosmongodb"
+  resource_group_name = azurerm_cosmosdb_account.acc.resource_group_name
+  account_name        = azurerm_cosmosdb_account.acc.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_mongo_collection" "coll" {
+  name                = "mrfirstcosmosmongodbcollection"
+  resource_group_name = azurerm_cosmosdb_account.acc.resource_group_name
+  account_name        = azurerm_cosmosdb_account.acc.name
+  database_name       = azurerm_cosmosdb_mongo_database.mongodb.name
+
+  default_ttl_seconds = "777"
+  shard_key           = "uniqueKey"
+  throughput          = 400
+
+  lifecycle {
+    ignore_changes = [index]
+  }
+
+  depends_on = [azurerm_cosmosdb_mongo_database.mongodb]
+
+
+}
---
+++ azure/cosmos-mongodb/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  default = "*********"
+}
+
+variable "resource_group_name" {
+  default = "multi-cloud-tooling"
+}
+
+variable "resource_group_location" {
+  default = "centralindia"
+}
+
+variable "cosmos_db_account_name" {
+  default = "mrnewcosmosmongodb"
+}
+
+variable "failover_location" {
+  default = "westindia"
+}
